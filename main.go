02d8c76aeb1304ddd58f3b5e9e889ad5f87ba73c
Vinicius Renó <vinicius.vbreno@gmail.com>
2020-11-10 08:24:28 -0300

    modified to use background image

--- main.go
+++ main.go
@@ -7,16 +7,37 @@ import (
 	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
+// Create our empty vars
+var (
+	err        error
+	background *ebiten.Image
+)
+
+// Run this code once at startup
+func init() {
+	background, _, err = ebitenutil.NewImageFromFile("assets/space.png", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Update the screen
 func update(screen *ebiten.Image) error {
 	if ebiten.IsDrawingSkipped() {
 		return nil
 	}
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(0, 0)
+	if err := screen.DrawImage(background, op); err != nil {
+		log.Fatal(err)
+	}
+
 	return nil
 }
 
+// Main loop
 func main() {
-	if err := ebiten.Run(update, 320, 240, 2, "Hello, World!"); err != nil {
+	if err := ebiten.Run(update, 640, 480, 1, "Hello, World!"); err != nil {
 		log.Fatal(err)
 	}
 }
